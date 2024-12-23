c03a05ad86ffb24e53c67dea3d700cf8d6a54c70
motexture <64956941+motexture@users.noreply.github.com>
2024-12-23 20:01:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -13,8 +13,6 @@ local_dir = "./model"
 
 snapshot_download(repo_id, local_dir=local_dir)
 
-WIDTH = 320
-HEIGHT = 320
 NUM_FRAMES = 8
 
 def set_seed(seed):
@@ -24,7 +22,7 @@ def set_seed(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    
+
 class VideoGenerator:
     def __init__(self):
         self.device = "cuda"
@@ -34,32 +32,46 @@ class VideoGenerator:
         self.generated = False
         self.video_path = "concatenated_output.mp4"
 
-    def initialize_pipeline(self, model):  
+    def initialize_pipeline(self, model):
         print("Loading pipeline...")
         pipeline = caTPipeline.from_pretrained(pretrained_model_name_or_path=local_dir).to(self.device)
-        #pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, timestep_spacing="trailing", algorithm_type="sde-dpmsolver++")
         pipeline.vae.enable_slicing()
+        return pipeline
+
+    def set_scheduler(self, pipeline, solver_type):
+        if solver_type == "DDIM":
+            pipeline.scheduler = pipeline.scheduler  # Default scheduler
+        elif solver_type == "DPM++ 2M":
+            pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+        elif solver_type == "DPM++ 2M Karras":
+            pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, use_karras_sigmas=True)
+        elif solver_type == "DPM++ 2M SDE":
+            pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, algorithm_type="sde-dpmsolver++")
+        elif solver_type == "DPM++ 2M SDE Karras":
+            pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, use_karras_sigmas=True, algorithm_type="sde-dpmsolver++")
 
         return pipeline
-    
+
     def match_histogram(self, source, target):
         """Adjust the color histogram of the target to match the source."""
         mean_src = source.mean(dim=(2, 3, 4), keepdim=True)
         std_src = source.std(dim=(2, 3, 4), keepdim=True)
-        
+
         mean_tgt = target.mean(dim=(2, 3, 4), keepdim=True)
         std_tgt = target.std(dim=(2, 3, 4), keepdim=True)
-        
+
         adjusted_target = (target - mean_tgt) * (std_src / (std_tgt + 1e-5)) + mean_src
         return adjusted_target
 
-    def generate(self, prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps):
+    def generate(self, prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, width, height, solver_type):
+        self.pipeline = self.set_scheduler(self.pipeline, solver_type)
+
         with torch.no_grad(), torch.autocast(self.device):
             latents = self.pipeline(
                 prompt=prompt,
                 negative_prompt=negative_prompt,
-                width=WIDTH,
-                height=HEIGHT,
+                width=width,
+                height=height,
                 num_frames=NUM_FRAMES,
                 device=self.device,
                 num_inference_steps=num_inference_steps,
@@ -74,26 +86,26 @@ class VideoGenerator:
             self.save_video(self.decode(self.stacked_latents), self.video_path, fps)
 
             return self.video_path
-    
+
     def normalize_latents(self, latents):
         mean = latents.mean()
         std = latents.std()
         normalized_latents = (latents - mean) / (std + 1e-8)
         return normalized_latents
-    
+
     def denormalize(self, normalized_tensor):
         if normalized_tensor.is_cuda:
             normalized_tensor = normalized_tensor.cpu()
-        
+
         if normalized_tensor.dim() == 5:
             normalized_tensor = normalized_tensor.squeeze(0)
-            
+
         denormalized = (normalized_tensor + 1.0) * 127.5
         denormalized = torch.clamp(denormalized, 0, 255)
-        
+
         uint8_tensor = denormalized.to(torch.uint8)
         uint8_numpy = uint8_tensor.permute(1, 2, 3, 0).numpy()
-        
+
         return uint8_numpy
 
     def save_video(self, normalized_tensor, output_path, fps=30):
@@ -102,11 +114,11 @@ class VideoGenerator:
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-        
+
         for frame in denormalized_frames:
             bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             out.write(bgr_frame)
-        
+
         out.release()
 
     def decode(self, latents):
@@ -128,9 +140,9 @@ class VideoGenerator:
             .permute(0, 2, 1, 3, 4)
         )
 
-        return video.float()        
+        return video.float()
 
-    def reset_and_generate_initial(self, prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, seed):
+    def reset_and_generate_initial(self, prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, seed, width, height, solver_type):
         self.stacked_latents = None
         self.previous_latents = None
         self.generated = False
@@ -139,7 +151,7 @@ class VideoGenerator:
         else:
             set_seed(random.randint(1, 1000000))
 
-        return self.generate(prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps)
+        return self.generate(prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, width, height, solver_type)
 
 video_gen = VideoGenerator()
 with gr.Blocks() as iface:
@@ -153,21 +165,26 @@ with gr.Blocks() as iface:
             guidance_scale = gr.Slider(label="Guidance Scale", minimum=1.0, maximum=30.0, step=0.1, value=14)
             fps = gr.Slider(label="FPS", minimum=1, maximum=60, step=1, value=16)
 
+            width = gr.Slider(label="Width", minimum=192, maximum=448, step=64, value=320)
+            height = gr.Slider(label="Height", minimum=192, maximum=448, step=64, value=320)
+
+            solver_type = gr.Dropdown(label="Solver Type", choices=["DDIM", "DPM++ 2M", "DPM++ 2M Karras", "DPM++ 2M SDE", "DPM++ 2M SDE Karras"], value="DDIM")
+
             seed = gr.Slider(label="Seed", minimum=-1, maximum=1000000, step=1, value=-1)
         with gr.Column():
-            video_output = gr.Video(label="Generated Video", width=WIDTH, height=HEIGHT)
+            video_output = gr.Video(label="Generated Video", width=320, height=320)
             generate_initial_button = gr.Button("Generate Initial Video")
             extend_button = gr.Button("Extend Video")
 
         generate_initial_button.click(
             video_gen.reset_and_generate_initial, 
-            inputs=[prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, seed],
+            inputs=[prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, seed, width, height, solver_type],
             outputs=video_output
         )
 
         extend_button.click(
             video_gen.generate, 
-            inputs=[prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps],
+            inputs=[prompt, negative_prompt, interpolation_strength, num_inference_steps, guidance_scale, fps, width, height, solver_type],
             outputs=video_output
         )
 
