adb8931e5f9defa98b3f4c4e0f8dbbaae066881c
motexture <64956941+motexture@users.noreply.github.com>
2025-01-12 01:53:06 +0300

    Update run.py

--- run.py
+++ run.py
@@ -159,7 +159,7 @@ with gr.Blocks() as iface:
         with gr.Column():
             prompt = gr.Textbox(label="Prompt", value="Darth Vader is surfing on the ocean, slow mo")
             negative_prompt = gr.Textbox(label="Negative Prompt", value="malformed, fast motion, low quality, worse quality, blurry, watermark")
-            interpolation_strength = gr.Slider(label="Interpolation Strength", minimum=0.0, maximum=1.0, step=0.1, value=0.5)
+            interpolation_strength = gr.Slider(label="Interpolation Strength", minimum=0.0, maximum=1.0, step=0.1, value=0.0)
 
             num_inference_steps = gr.Slider(label="Number of Inference Steps", minimum=1, maximum=150, step=1, value=20)
             guidance_scale = gr.Slider(label="Guidance Scale", minimum=1.0, maximum=30.0, step=0.1, value=7.5)
